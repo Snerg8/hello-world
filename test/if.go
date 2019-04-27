1a5e181ffe18dbfb8ef5be523fff6da802750fb7
Snerg8 <snerg8@gmail.com>
2019-04-27 15:24:22 +0300

    new

---
+++ test/if.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main(){
+
+for i := 1; i <= 100; i++{
+if i % 3 == 0{
+	fmt.Println(i, "odd")
+
+} else {
+
+	fmt.Println(i, "mistake")
+	}
+
+}
+
+/*for j :=1;  j <=10;  j++ {
+
+	if j % 2 == 0 {
+	fmt.Println(j, "even")	
+	
+	 } else {  
+	
+	fmt.Println(j, "odd")
+	}
+    }*/
+
+}
+
+
+/*
+	j :=10
+	
+	if j >= 10 {
+		fmt.Println("Big")
+	
+	} else {
+		fmt.Println("Small")
+*/
+
+
+
